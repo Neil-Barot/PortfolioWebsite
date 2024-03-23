26d6715273c3cc392a21502cf178816a41f0a9fb
Neil-Barot <neil.r.barot@gmail.com>
2024-03-23 05:02:16 -0500

    Interactive Resums: Content yet to be added

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -7,7 +7,22 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Neil Barot - Resume');
+  }
+
+  isEducationOpen: boolean = false;
+  isSkillsOpen: boolean = false;
+  isWorkExperienceOpen: boolean = false;
+  isProjectsOpen: boolean = false;
+
+
+  DownloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/ResumeV6.pdf');
+    link.setAttribute('download', 'Resume.pdf');
+    link.click();
+    link.remove();
   }
 }
